738df1dbde66ad9d79bc9d4b74dc2531a12b2ce0
Dan Blossom <daniel.blossom@gmail.com>
2013-09-12 07:28:49 -0400

    answers to homework 1 questions 1 - 10

--- homework1.sql
+++ homework1.sql
@@ -1,12 +1,58 @@
--- Homework 1
--- Dan Blossom
+--Dan Blossom
 
--- 1
-SELECT * 
+--CMPT 308 Database
+
+--Professor Labouseur
+
+--1
+﻿SELECT *
 FROM customers;
 
---adding something
+--2
+﻿SELECT name, city
+FROM agents
+WHERE name = 'Smith';
 
--- blah
+--3
+﻿SELECT pid, name, quantity
+FROM products
+WHERE priceUSD > 1.25;
 
--- one more thing
+--4
+﻿SELECT ordno, aid
+FROM orders;
+
+--5
+﻿SELECT name, city
+FROM customers
+WHERE city <> 'Dallas';
+
+--6
+﻿SELECT name
+FROM agents
+WHERE city = 'Newark'
+OR city = 'New York';
+
+--7
+﻿SELECT *
+FROM products
+WHERE city <> 'Newark'
+AND city <> 'New York'
+AND priceUSD <= 1.00;
+
+--8
+﻿SELECT *
+FROM orders
+WHERE mon = 'jan'
+OR mon = 'mar';
+
+--9
+﻿SELECT *
+FROM orders
+WHERE mon = 'feb'
+AND dollars < 100.00;
+
+--10
+﻿SELECT *
+FROM orders
+WHERE cid = 'c005'
